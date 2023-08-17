896fa472cf484f165d7b07c994283e48d583d954
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2023-08-17 21:34:26 +0100

    Tidying

--- src/levels.h
+++ src/levels.h
@@ -10,6 +10,9 @@ typedef struct {
     u8 levelMap[17][32]; //20 levels each 17 x 32 tiles (8x8 tile size)
     Sprite *lvSprites[8]; //animated tiles
 } levelData;
+const Image *levelsBG[20] = {&level1, &level2, &level3, &level4, &level5, &level6, &level7, &level8,
+                            &level9, &level10, &level11, &level12, &level13, &level14, &level15, &level16,
+                            &level17, &level18, &level19, &level20};
 char *levelNames[] ={
 	"Central Cavern",
 	"The Cold Room",
